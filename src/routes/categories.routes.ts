de7a36e96e1970dbb0bf6cac52f24c68ced4b70e
Matheus Araújo <souzamatheus8174@gmail.com>
2022-03-12 16:07:28 -0300

    feat: update categories routes add /import post route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,32 @@
+import multer, { memoryStorage } from 'multer';
 import { Router } from 'express';
 
 import createCategoryController from '../modules/cars/useCases/createCategory';
 import listCategoriesController from '../modules/cars/useCases/listCategories';
+import importCategoryController from '../modules/cars/useCases/importCategory';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  fileFilter: (request, file, callback) => {
+    const [extension, ,] = file.originalname.split('.').reverse();
+
+    if (extension !== 'csv') callback(new Error('Just csv files are allowed'));
+
+    callback(null, true);
+  },
+  storage: memoryStorage(),
+  dest: './tmp'
+});
 
 categoriesRoutes.post('/', createCategoryController.execute);
 
 categoriesRoutes.get('/', listCategoriesController.execute);
+
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  importCategoryController.execute
+);
 
 export default categoriesRoutes;

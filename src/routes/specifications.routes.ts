6169f093016d73929e4b9257da0a54e5cafb2839
Matheus Araújo <souzamatheus8174@gmail.com>
2022-03-12 16:10:30 -0300

    refactor: update specification route remove controller dependence

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,26 +1,9 @@
 import { Router } from 'express';
 
-import SpecificationsRepository from '../modules/cars/repositories/SpecificationsRepository';
-import CreateSpecificationService from '../modules/cars/services/CreateSpecificationService';
+import createSpecificationController from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
-specificationsRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  try {
-    createSpecificationService.execute({ name, description });
-  } catch ({ message }) {
-    return response.status(400).json({ message });
-  }
-
-  return response.status(201).end();
-});
+specificationsRoutes.post('/', createSpecificationController.execute);
 
 export default specificationsRoutes;

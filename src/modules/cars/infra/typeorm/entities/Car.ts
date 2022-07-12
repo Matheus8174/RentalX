6c092bd94d43403ae501322a98538cb1ea86607a
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 16:19:46 -0300

    feat: create car model

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,65 @@
+import { randomUUID as uuidV4 } from 'crypto';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from 'typeorm';
+
+import Category from './Category';
+import Specification from './Specification';
+
+@Entity('cars')
+class Car {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  daily_rate: number;
+
+  @Column({ default: true })
+  available: boolean;
+
+  @Column()
+  license_plate: string;
+
+  @Column()
+  fine_amount: number;
+
+  @Column()
+  brand: string;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
+  category_id: string;
+
+  @ManyToMany(() => Specification, { cascade: true })
+  @JoinTable({
+    name: 'specifications_cars',
+    joinColumns: [{ name: 'car_id' }],
+    inverseJoinColumns: [{ name: 'specification_id' }]
+  })
+  specifications: Specification[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+  }
+}
+
+export default Car;

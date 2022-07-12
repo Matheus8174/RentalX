4ffa957fadb316aee3166f9f38b77567e17c6a36
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 15:10:48 -0300

    refactor: change database config to use a commom configuration

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,18 +1,18 @@
 import '@config/env';
+
 import { ConnectionOptions } from 'typeorm';
 
+import baseConfigConnection from '@shared/infra/typeorm/baseConfigConnection';
+
 const connectionOptions: ConnectionOptions = {
+  ...baseConfigConnection,
+
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  entities: ['./src/modules/**/entities/*.ts'],
-  migrations: ['./src/database/migrations/*.ts'],
-  cli: {
-    migrationsDir: './src/database/migrations'
-  }
+  database: process.env.POSTGRES_DB
 };
 
 export default connectionOptions;

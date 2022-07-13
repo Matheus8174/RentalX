53cf25e6241536f97ad5fc1aab8b2499ebbe5ea9
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 21:09:38 -0300

    feat: change folder

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,15 +2,15 @@ import 'reflect-metadata';
 import 'express-async-errors';
 
 import '@config/env';
-import './database/connection';
+import '@shared/infra/typeorm/connection';
 import '@shared/container';
 
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
-import swaggerFile from './docs/swagger.json';
+import swaggerFile from '../../../docs/swagger.json';
 import routes from './routes';
-import handleError from './middlewares/handleError';
+import handleError from '@shared/infra/http/middlewares/handleError';
 
 const app = express();
 

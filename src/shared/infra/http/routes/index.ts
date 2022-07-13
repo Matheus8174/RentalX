0bda47d70a8ed40c515783c290b2c4b395c9d328
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 22:03:36 -0300

    refactor: chage folder

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import carsRoutes from './cars.routes';
 import categoriesRoutes from './categories.routes';
 import specificationsRoutes from './specifications.routes';
 import usersRoutes from './users.routes';
 import authenticateRoutes from './authenticate.routes';
+import rentalRoutes from './rental.routes';
 
 const routes = Router();
 
@@ -13,5 +15,7 @@ routes.use(`${prefixRoutes}/categories`, categoriesRoutes);
 routes.use(`${prefixRoutes}/specifications`, specificationsRoutes);
 routes.use(`${prefixRoutes}/users`, usersRoutes);
 routes.use(`${prefixRoutes}/session`, authenticateRoutes);
+routes.use(`${prefixRoutes}/cars`, carsRoutes);
+routes.use(`${prefixRoutes}/rentals`, rentalRoutes);
 
 export default routes;

83f70bb343f481d6e06f6a46d92b15cb3304a872
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 22:26:38 -0300

    feat: create listAvailableCars entry point

---
+++ src/modules/cars/useCases/listAvailableCars/index.ts
@@ -0,0 +1,5 @@
+import ListAvailableCarsController from './ListAvailableCarsController';
+
+const listAvailableCarsController = new ListAvailableCarsController();
+
+export default listAvailableCarsController.execute;

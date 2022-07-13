f3c6302ecce7f89dc3ee1eb79fe4a2d9881bf3b2
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 22:09:58 -0300

    feat: create cars repository interface

---
+++ src/modules/cars/repositories/interfaces/ICarsRepository.ts
@@ -0,0 +1,22 @@
+import ICreateCarDTO from '@modules/cars/dtos/ICreateCarDTO';
+import Car from '@modules/cars/infra/typeorm/entities/Car';
+
+export type FindAvailableParams = {
+  brand?: string;
+  category_id?: string;
+  name?: string;
+};
+
+interface ICarsRepository {
+  findAvailable({
+    name,
+    brand,
+    category_id
+  }: FindAvailableParams): Promise<Car[]>;
+  create(data: ICreateCarDTO): Promise<Car>;
+  findByLicensePlate(licensePlate: string): Promise<Car | undefined>;
+  findById(carId: string): Promise<Car | undefined>;
+  save(data: ICreateCarDTO): Promise<any>;
+}
+
+export default ICarsRepository;

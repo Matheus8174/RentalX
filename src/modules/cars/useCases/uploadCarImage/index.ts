99665df27171f88be53210816384ab915d2bf053
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 22:26:55 -0300

    feat: create uploadCarIMage entry point

---
+++ src/modules/cars/useCases/uploadCarImage/index.ts
@@ -0,0 +1,5 @@
+import UploadCarImagesController from './UploadCarImagesController';
+
+const uploadCarImagesController = new UploadCarImagesController();
+
+export default uploadCarImagesController.execute;

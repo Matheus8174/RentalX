22960b0db152e4f912200d64294ad3274b0ddbf3
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 22:24:19 -0300

    feat: create createDatabaseConnection helper

---
+++ tests/helpers/createDatabaseConnection.ts
@@ -0,0 +1,23 @@
+import { createConnection } from 'typeorm';
+
+import baseConfigConnection from '@shared/infra/typeorm/baseConfigConnection';
+
+async function createDatabaseConnection() {
+  const connection = await createConnection({
+    ...baseConfigConnection,
+    type: 'postgres',
+    name: 'test',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+
+    migrationsRun: true,
+    dropSchema: true
+  });
+
+  return connection;
+}
+
+export default createDatabaseConnection;

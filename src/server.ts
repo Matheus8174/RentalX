6f582dce85d7af65ef16992511e8325bd85f8122
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 21:37:17 -0300

    refactor: update to use typescript paths and graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,29 @@
-import app from './app';
+import { getConnection } from 'typeorm';
+
+import app from '@shared/infra/http/app';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port);
+const server = app.listen(port);
+
+// Disparado no Ctrl + C no terminal -> multi plataforma
+process.on('SIGINT', () => {
+  server.close(() => {
+    getConnection()
+      .close()
+      .then(() => {
+        process.exit(0);
+      });
+  });
+});
+
+// Disparado no kill
+process.on('SIGTERM', () => {
+  server.close(() => {
+    getConnection()
+      .close()
+      .then(() => {
+        process.exit(0);
+      });
+  });
+});

782def63f99c7e7f452bd4983b1f33bb41ffdf5d
Matheus Araújo <souzamatheus8174@gmail.com>
2022-07-12 22:27:29 -0300

    feat: create containers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,13 +1,29 @@
+import '@shared/container/providers';
+
 import { container } from 'tsyringe';
 
 import ICategoriesRepository from '@modules/cars/repositories/interfaces/ICategoriesRepository';
-import CategoriesRepository from '@modules/cars/repositories/CategoriesRepository';
+import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 
 import ISpecificationsRepository from '@modules/cars/repositories/interfaces/ISpecificationsRepository';
-import SpecificationsRepository from '@modules/cars/repositories/SpecificationsRepository';
+import SpecificationsRepository from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 
 import IUsersRepository from '@modules/accounts/repositories/interfaces/IUsersRepository';
-import UsersRepository from '@modules/accounts/repositories/UsersRepository';
+import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+
+import ICarsRepository from '@modules/cars/repositories/interfaces/ICarsRepository';
+import CarsRepository from '@modules/cars/infra/typeorm/repositories/CarsRepository';
+
+import ICarImagesRepository from '@modules/cars/repositories/interfaces/ICarImagesRepository';
+import CarsImageRepository from '@modules/cars/infra/typeorm/repositories/CarsImageRepository';
+
+import IRentalsRepository from '@modules/rentals/repositories/IRentalsRepository';
+import RentalsRepository from '@modules/rentals/infra/typeorm/repositories/RentalsRepository';
+
+container.registerSingleton<ICarImagesRepository>(
+  'CarsImagesRepository',
+  CarsImageRepository
+);
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -22,4 +38,11 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UserRepository',
   UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+
+container.registerSingleton<IRentalsRepository>(
+  'RentalsRepository',
+  RentalsRepository
 );

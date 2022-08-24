8353b8a9cf985e454e86212564fa91f943e310bb
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:21:34 -0300

    refactor: update ensureAuthenticated middleware

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import AppError from '@shared/errors/AppError';
-import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 
 async function ensureAuthenticated(
   request: Request,
@@ -18,18 +17,12 @@ async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(token, process.env.JWT_SECRET);
 
-    const userRepository = new UsersRepository();
-
-    const user = await userRepository.findById(user_id as string);
-
-    if (!user) throw new AppError('User does not exists', 401);
-
     request.user = {
       id: user_id as string
     };
 
     next();
-  } catch {
+  } catch (error) {
     throw new AppError('Invalid token', 401);
   }
 }

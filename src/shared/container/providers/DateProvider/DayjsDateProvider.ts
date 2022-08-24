9a2ae39b2da46e2dbfc289ea3270f9e13444f6b9
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:18:48 -0300

    refactor: update dayjs provider

--- src/shared/container/providers/DateProvider/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/DayjsDateProvider.ts
@@ -20,6 +20,25 @@ class DayjsDateProvider implements IDateProvider {
   public dateNow() {
     return dayjs().toDate();
   }
+
+  public compareInDays(startDate: Date, endDate: Date) {
+    const endDateUtc = this.convertToUTC(endDate);
+    const startDateUtc = this.convertToUTC(startDate);
+
+    return dayjs(endDateUtc).diff(startDateUtc, 'days');
+  }
+
+  public addDays(days: number) {
+    return dayjs().add(days, 'days').toDate();
+  }
+
+  public addHours(hours: number) {
+    return dayjs().add(hours, 'days').toDate();
+  }
+
+  public compareIfBefore(startDate: Date, endDate: Date) {
+    return dayjs(startDate).isBefore(endDate);
+  }
 }
 
 export default DayjsDateProvider;

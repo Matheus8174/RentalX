49bfd697ae2636726538e4e5bb54c622a2d56b67
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:47:16 -0300

    refactor: update authenticate route

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import authenticateUserController from '@modules/accounts/useCases/authenticateUser';
+import refreshTokenController from '@modules/accounts/useCases/refreshToken';
 
 const authenticateRoutes = Router();
 
 authenticateRoutes.post('/', authenticateUserController);
+authenticateRoutes.post('/refresh-token', refreshTokenController);
 
 export default authenticateRoutes;

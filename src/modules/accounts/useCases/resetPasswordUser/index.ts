b769ece23009e5803fe040c0bd33d9e2663a07ea
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 13:45:15 -0300

    feat: create resetPasswordUser entry point

---
+++ src/modules/accounts/useCases/resetPasswordUser/index.ts
@@ -0,0 +1,5 @@
+import RefreshTokenController from '../refreshToken/RefreshTokenController';
+
+const refreshTokenController = new RefreshTokenController();
+
+export default refreshTokenController.execute;

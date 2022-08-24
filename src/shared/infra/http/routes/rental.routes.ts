f542a2441d800bf0c94409e74ee6463b8f854289
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:47:44 -0300

    refactor: update rental route

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 
 import createRentalController from '@modules/rentals/usecases/createRental';
+import devolutionRental from '@modules/rentals/usecases/devolutionRental';
+import listRentalsByUser from '@modules/rentals/usecases/listRentalsByUser';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const rentalRoutes = Router();
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController);
+rentalRoutes.post('/devolution/:id', ensureAuthenticated, devolutionRental);
+rentalRoutes.get('/user', ensureAuthenticated, listRentalsByUser);
 
 export default rentalRoutes;

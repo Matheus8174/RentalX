8a49b678454040ca3b18c99ed1ae189c28418a85
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:51:46 -0300

    refactor: update routes entry point

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,6 +6,7 @@ import specificationsRoutes from './specifications.routes';
 import usersRoutes from './users.routes';
 import authenticateRoutes from './authenticate.routes';
 import rentalRoutes from './rental.routes';
+import passwordRoutes from './password.routes';
 
 const routes = Router();
 
@@ -17,5 +18,6 @@ routes.use(`${prefixRoutes}/users`, usersRoutes);
 routes.use(`${prefixRoutes}/session`, authenticateRoutes);
 routes.use(`${prefixRoutes}/cars`, carsRoutes);
 routes.use(`${prefixRoutes}/rentals`, rentalRoutes);
+routes.use(`${prefixRoutes}/password`, passwordRoutes);
 
 export default routes;

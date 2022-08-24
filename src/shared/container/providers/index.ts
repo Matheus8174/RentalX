620a04d874cd0799d3c6b580dd2f15508c5a1865
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:09:31 -0300

    feat: update provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import DayjsDateProvider from './DateProvider/DayjsDateProvider';
 import IDateProvider from './DateProvider/interfaces/IDateProvider';
 
+import EtherealMailProvider from './MailProvider/EtherealMailProvider';
+import IMailProvider from './MailProvider/interfaces/IMailProvider';
+
 container.registerSingleton<IDateProvider>(
   'DayjsDateProvider',
   DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  'EtherealMailProvider',
+  new EtherealMailProvider()
 );

c9553cdab00b4734b5d79a235eaa4a770a3e21cc
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 12:59:25 -0300

    chore: add refresh token

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -6,8 +6,13 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test';
       PORT: number | string;
+
       JWT_SECRET: Secret;
       EXPIRES_IN: string | number;
+
+      EXPIRES_REFRESH_TOKEN_DAYS: number;
+      SECRET_REFRESH_TOKEN: Secret;
+      EXPIRES_IN_REFRESH_TOKEN: string | number;
 
       POSTGRES_USER: string;
       POSTGRES_PASSWORD: string;
@@ -15,6 +20,8 @@ declare global {
 
       ADMIN_PASSWORD: string;
       ADMIN_EMAIL: string;
+
+      FORGOT_MAIL_URL: string;
     }
   }
 }

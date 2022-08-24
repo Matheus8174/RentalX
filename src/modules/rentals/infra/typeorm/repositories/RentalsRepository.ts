706c4b7eef34e860c7c26dd5eecb4c4171122fa9
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 15:27:10 -0300

    refactor: update Rental repository

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -1,7 +1,9 @@
 import { getRepository, Repository } from 'typeorm';
 
 import ICreateRentalDTO from '@modules/rentals/dtos/ICreateRentalDTO';
-import IRentalsRepository from '@modules/rentals/repositories/IRentalsRepository';
+import IRentalsRepository, {
+  ISaveRental
+} from '@modules/rentals/repositories/IRentalsRepository';
 
 import Rental from '../entities/Rental';
 
@@ -13,27 +15,58 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   public async findOpenRentalByCar(carId: string) {
-    const rental = await this.repository.findOne({ car_id: carId });
+    const rental = await this.repository.findOne({
+      where: { car_id: carId, end_date: null }
+    });
 
     return rental;
   }
 
   public async findOpenRentalByUser(userId: string) {
-    const rental = await this.repository.findOne({ user_id: userId });
+    const rental = await this.repository.findOne({
+      where: { user_id: userId, end_date: null }
+    });
 
     return rental;
   }
 
-  public async create({ carId, userId, expectedReturnDate }: ICreateRentalDTO) {
+  public async create({
+    carId,
+    userId,
+    expectedReturnDate,
+    total,
+    id
+  }: ICreateRentalDTO) {
     const rental = this.repository.create({
       car_id: carId,
       user_id: userId,
-      expected_return_date: expectedReturnDate
+      expected_return_date: expectedReturnDate,
+      total,
+      id
     });
 
     await this.repository.save(rental);
 
     return rental;
+  }
+
+  public async findById(id: string) {
+    const rental = await this.repository.findOne(id);
+
+    return rental;
+  }
+
+  public async save(data: ISaveRental) {
+    await this.repository.save(data);
+  }
+
+  public async findByUser(userId: string) {
+    const rentals = await this.repository.find({
+      where: { user_id: userId },
+      relations: ['car']
+    });
+
+    return rentals;
   }
 }
 

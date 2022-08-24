2744b71ad071f7cf30bec8ad7449b850be880aa7
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 15:27:34 -0300

    refactor: update Rental repository interface

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,10 +1,18 @@
 import Rental from '@modules/rentals/infra/typeorm/entities/Rental';
 import ICreateRentalDTO from '../dtos/ICreateRentalDTO';
 
+export type ISaveRental = Omit<
+  ICreateRentalDTO,
+  'userId' | 'carId' | 'expectedReturnDate'
+>;
+
 interface IRentalsRepository {
   findOpenRentalByCar(caId: string): Promise<Rental | undefined>;
   findOpenRentalByUser(userId: string): Promise<Rental | undefined>;
   create(data: ICreateRentalDTO): Promise<Rental>;
+  save(data: ISaveRental): Promise<void>;
+  findById(id: string): Promise<Rental | undefined>;
+  findByUser(userId: string): Promise<Rental[]>;
 }
 
 export default IRentalsRepository;

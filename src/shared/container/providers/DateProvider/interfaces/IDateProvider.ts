914722fbf3697344912cc56a3bdccd592efc3c69
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:15:56 -0300

    refactor: update date provider

--- src/shared/container/providers/DateProvider/interfaces/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/interfaces/IDateProvider.ts
@@ -2,6 +2,10 @@ interface IDateProvider {
   compareInHours(startDate: Date, endDate: Date): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
+  compareInDays(startDate: Date, endDate: Date): number;
+  addDays(days: number): Date;
+  addHours(hours: number): Date;
+  compareIfBefore(startDate: Date, endDate: Date): boolean;
 }
 
 export default IDateProvider;

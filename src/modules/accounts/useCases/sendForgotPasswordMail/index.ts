c16cf1a041f0e87aaec3ab94879f4f9d53477440
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 13:47:29 -0300

    feat: create sendForgotPasswordMail entry point

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/index.ts
@@ -0,0 +1,5 @@
+import SendForgotPasswordMailController from './SendForgotPasswordMailController';
+
+const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+
+export default sendForgotPasswordMailController.execute;

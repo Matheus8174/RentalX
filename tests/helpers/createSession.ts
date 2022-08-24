8342e899cac0e5902fbe222287bae3ec0cd39166
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 16:35:36 -0300

    refactor: update createSession test helper

--- tests/helpers/createSession.ts
+++ tests/helpers/createSession.ts
@@ -1,10 +1,19 @@
 import AuthenticateUserUseCase from '@modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase';
+
 import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import UsersTokensRepository from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import DayjsDateProvider from '@shared/container/providers/DateProvider/DayjsDateProvider';
 
 async function createSession() {
   const usersRepository = new UsersRepository();
+  const usersTokensRepository = new UsersTokensRepository();
+  const dayjsDateProvider = new DayjsDateProvider();
 
-  const authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
+  const authenticateUserUseCase = new AuthenticateUserUseCase(
+    usersRepository,
+    usersTokensRepository,
+    dayjsDateProvider
+  );
 
   const session = await authenticateUserUseCase.execute({
     email: process.env.ADMIN_EMAIL,

3fefad6969faedd9a0329d8e7226a6a3ac2572a2
Matheus Araújo <souzamatheus8174@gmail.com>
2022-08-24 15:51:24 -0300

    feat: create listRentalByUser entry point

---
+++ src/modules/rentals/usecases/listRentalsByUser/index.ts
@@ -0,0 +1,5 @@
+import ListRentalsByUserController from './ListRentalsByUserController';
+
+const listRentalsByUserController = new ListRentalsByUserController();
+
+export default listRentalsByUserController.execute;
